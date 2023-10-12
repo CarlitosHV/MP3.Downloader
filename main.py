83c13c578227e5ea2f346eee15dc55248fc3d35b
CarlitosHV <carloshvsa21@hotmail.com>
2023-10-12 14:07:36 -0600

    Modifiqué y agregué la librería MoviePy para convertir el archivo a MP3, ya que había bug que algunos MP3 salían corruptos

--- main.py
+++ main.py
@@ -1,55 +1,60 @@
 from pytube import YouTube
+from moviepy.editor import *
 import os
 import re
 
 
 def format_video_title(title):
-    # Reemplazamos guiones bajos y quitamos el símbolo | por si lo tiene
-    words = title.split("_")
-    formatted_title = " ".join(word.capitalize() for word in words)
-
-    formatted_title = re.sub(r'[\\/:"*?<>|]', '', formatted_title)
+    # Reemplazamos guiones bajos y quitamos caracteres no permitidos
+    formatted_title = re.sub(r'[\\/:"*?<>|]', '', title)
     return formatted_title
 
 
 while True:
     # Link que puso el usuario
-    url = input(
-        "Ingresa el enlace del video de YouTube que deseas descargar y convertir a MP3 (o 'salir' para terminar):\n>> ")
+    url = input("Ingresa el enlace del video de YouTube que deseas descargar y convertir a MP3 (o 'salir' para terminar):\n>> ")
 
     if url.lower() == 'salir':
         break
 
     try:
         yt = YouTube(url)
-    except:
-        print("Error: No se pudo obtener el video desde el enlace proporcionado.")
+    except Exception as e:
+        print(f"Error: No se pudo obtener el video desde el enlace proporcionado. Error: {str(e)}")
         continue
-
-    # Extraemos solo el audio del video
-    video = yt.streams.filter(only_audio=True).first()
 
     # Preguntamos la ruta de guardado del archivo
     destination = input("Ingresa la ruta de destino (Dejar en blanco para el directorio actual):\n") or '.'
 
     # Descargamos el archivo
-    out_file = video.download(output_path=destination)
+    try:
+        out_file = yt.streams.filter(only_audio=True, file_extension='mp4', abr='128kbps').first().download(output_path=destination)
+    except Exception as e:
+        print(f"Error al descargar el archivo. Error: {str(e)}")
+        continue
 
     # Obtenemos el nombre del archivo formateado
     video_title = format_video_title(yt.title)
 
-    # Creamos el nuevo nombre con el siguiente formato: NombreArtista - NombreCancion.mp3
-    # No olvides reemplazar NombreArtista por el que estás descargando o puedes personalizar el título
-    new_filename = f'Artista - {video_title}.mp3'
+    # Creamos el nuevo nombre
+    new_filename = f'{video_title}.mp3'
 
     # Construimos el path final del archivo
     new_file_path = os.path.join(destination, new_filename)
 
-    # Renombramos y movemos el archivo final
-    os.rename(out_file, new_file_path)
+    # Convertimos el archivo a formato MP3
+    try:
+        audio = AudioFileClip(out_file)
+        audio.write_audiofile(new_file_path)
+    except Exception as e:
+        print(f"Error al convertir a MP3. Error: {str(e)}")
+        continue
+
+    # Eliminamos el archivo descargado en formato mp4
+    os.remove(out_file)
 
     # Imprimimos un resultado de completado
-    print(yt.title + " se ha descargado completamente como " + new_filename)
+    print(yt.title + " se ha descargado y convertido a MP3 correctamente como " + new_filename)
 
     # Preguntamos si quiere seguir descargando más archivos MP3
     choice = input("¿Deseas continuar descargando más videos? (s/n):\n")
